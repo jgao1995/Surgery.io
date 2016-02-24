25277342c01ebd91d752c4712213ea54e7705854
Sumit Shyamsukha <sumitzster@gmail.com>
2016-02-23 22:21:52 -0500

    added form to query catheters

--- catheters/plan_surgery/views.py
+++ catheters/plan_surgery/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'plan_surgery/index.html', {})
+  from django import forms
+  class NameForm(forms.Form):
+      your_name = forms.CharField(label='Enter catheter name ', max_length=100)
+
+  context = { "form" : NameForm() }
+  return render(request, 'plan_surgery/index.html', context)
