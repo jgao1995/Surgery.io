da9359172ea64883a5322ee83107be1e3cce2209
Raymond Yin <ryin@seas.upenn.edu>
2016-02-25 18:23:20 -0500

    search database

--- catheters/plan_surgery/views.py
+++ catheters/plan_surgery/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render_to_response, render
 from django.http import HttpResponse
+from django import forms
+from plan_surgery.models import Device, Surgery
+from collections import defaultdict
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Enter catheter name ', max_length=100)
 
 # Create your views here.
 def index(request):
-  from django import forms
-  class NameForm(forms.Form):
-      your_name = forms.CharField(label='Enter catheter name ', max_length=100)
 
-  context = { "form" : NameForm() }
-  return render(request, 'plan_surgery/index.html', context)
+    context = { "form" : NameForm() }
+    return render(request, 'plan_surgery/index.html', context)
+
+def search(request):
+    query = request.GET['query']
+    query_set = Device.objects.filter(manufacturer__contains=query) | Device.objects.filter(brand_name__contains=query) | Device.objects.filter(description__contains=query)
+    results = defaultdict(list)
+    for device in query_set:
+        results[device.product_type].append((device.manufacturer, device.brand_name, device.description, device.dimensions))
+    context = {'results': dict(results)}
+    return render(request, 'plan_surgery/search_results.html', context)
