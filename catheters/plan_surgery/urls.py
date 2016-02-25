ca6a3804f9ee5045b229cc2a619698485e879167
bettercallsaul12 <amitvpatel06@gmail.com>
2016-02-25 17:41:01 -0500

    all page

--- catheters/plan_surgery/urls.py
+++ catheters/plan_surgery/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^all/$', views.all, name="all"),
     url(r'^$', views.index, name='Index'),
-    url(r'^plan_surgery$', views.plan_surgery, name='Plan Surgery'),
+    url(r'^plan_surgery/$', views.plan_surgery, name='Plan Surgery'),
 ]
 
