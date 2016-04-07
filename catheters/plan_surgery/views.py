368c78c4645eff22a18e2f393ca6d987e6e9af6c
bettercallsaul12 <amitvpatel06@gmail.com>
2016-04-07 17:31:00 -0400

    code documetation

--- catheters/plan_surgery/views.py
+++ catheters/plan_surgery/views.py
@@ -19,7 +19,9 @@ class NameForm(forms.Form):
 
 #     context = { "form" : NameForm() }
 #     return render(request, 'plan_surgery/index.html', context)
-
+"""
+    Given a device, returns the compatible devices according to the rules defined in the Dependency database
+"""
 def compatibleCatheters(catheter):
     adjacency_list = DeviceDependency.filter(device_1=catheter)
     compatible = []
@@ -27,7 +29,10 @@ def compatibleCatheters(catheter):
         if(DeviceDependency.get(device_1=catheter, device_2=device) == 1):
             compatible.push(device.name + ':' + device.fields)
     return compatible
-
+"""
+    Search function that returns results when you press the search button. Finds any devices 
+    that have the query in their manufacturer, name, or description.
+"""
 
 def search(request):
     query = request.GET['query']
@@ -47,6 +52,10 @@ def search(request):
     context = {'results': dict(results)}
     return render(request, 'plan_surgery/search_results.html', context)
 
+"""
+    Method that returns a dictionary of matches for the query so far so that the frontend can display
+    dynamic search suggestions
+"""
 def dynamic_search(request):
     query = request.GET['query']
     query_set = Device.objects.filter(manufacturer__contains=query) | Device.objects.filter(brand_name__contains=query) | Device.objects.filter(description__contains=query)
@@ -63,10 +72,17 @@ def dynamic_search(request):
 
 from plan_surgery.models import *
 
-# Create your views here.
+# CViews are here.
+
+"""
+    Renders the homepage 
+"""
 def index(request):
     return render(request, 'index.html')
 
+"""
+    Renders the plan surgery page
+"""
 def plan_surgery(request):
     from django import forms
     class NameForm(forms.Form):
@@ -74,12 +90,16 @@ def plan_surgery(request):
 
     context = { "form" : NameForm() }
     return render(request, 'plan_surgery/index.html', context)
-
+"""
+    Renders the all catheters page
+"""
 def all(request):
     devices = Device.objects.all()      
     context = {"devices" : devices}
     return render(request, 'plan_surgery/all.html', context)
-
+"""
+    Renders the show page for a given catheter ID
+"""
 def show(request, id, message=""):
     device = Device.objects.get(pk=id)
     compatible = compatibleCatheters(device)
@@ -88,7 +108,9 @@ def show(request, id, message=""):
         links = [str(x).replace('watch?v=', 'v/') for x in json.loads(device.useful_links)]
     context = {"compatible_devices  ": compatible, "manufacturer": device.manufacturer, "brand_name": device.brand_name, "description": device.description, "product_type": device.product_type, "id": id, "notes": device.notes, "useful_links": links, "message": message}
     return render(request, 'plan_surgery/show.html', context)
-
+"""
+    Allows the user to add videos to a page through the add video button
+"""
 def add_video(request, id):
     device = Device.objects.get(pk=id)
     useful_links = device.useful_links
@@ -115,7 +137,9 @@ def add_video(request, id):
             return show(request, id, "Added video successfully.")
     return show(request, id, "Failed to add video. Please use a valid YouTube URL.")
 
-
+"""
+    Allows the user to add comments to a page through the add comment button
+"""
 def add_comment(request, id):
     device = Device.objects.get(pk=id)
     notes = request.GET['comment']
