2a14f8b5471442511e37a77c04feae0ae98325c1
Raymond Yin <ryin@seas.upenn.edu>
2016-04-25 23:32:00 -0400

    routing

--- catheters/plan_surgery/urls.py
+++ catheters/plan_surgery/urls.py
@@ -29,4 +29,7 @@ urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', views.log_in, name='login'),
     url(r'^logout/$', views.log_out, name='logout'),
+
+    # HELP
+    url(r'^help/$', views.help, name="help"),
 ]
